85c0f3923be0266df036347b015d447d5e22eb63
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-09-24 11:38:28 +0530

    v9

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // create an express app
 const express = require("express")
 const app = express()
+const { execSync } = require("child_process")
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<pre>" + execSync('yarn run cleanup').toString() + "</pre>");
 })
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000,
+  () => console.log("Server is running..."));
