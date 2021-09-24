4b873a4cdd0449d761ace344d86d11a44c864646
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-09-24 10:05:40 +0530

    v1

---
+++ mongodb-cleanup.ts
@@ -0,0 +1,84 @@
+import collectionPromise from './mongodb-client';
+
+export type doc = {ts: number; version: string; platform: string; count: number};
+
+export const cleanupDB = async (groupingPeriod: number) => {
+  console.log('Updating DB');
+  const client = await collectionPromise;
+  const collection = client.db('hyper').collection('log');
+  console.log('Connected to DB');
+  const data = await collection.findOne({
+    $expr: {
+      $and: [
+        {
+          $ne: [
+            {
+              $mod: ['$ts', groupingPeriod]
+            },
+            0
+          ]
+        },
+        {
+          $lt: [
+            '$ts',
+            new Date().getTime() - (3 * groupingPeriod)
+          ]
+        }
+      ]
+    }
+  });
+
+  if (!data) {
+    console.log('No data');
+    return client;
+  } else {
+    console.log('Fetched data');
+  }
+
+  const ts = data.ts - (data.ts % groupingPeriod);
+
+  console.log('processing', new Date(ts).toLocaleString());
+
+  const data2 = (
+    await collection
+      .find(
+        {
+          $expr: {
+            $eq: [{$subtract: ['$ts', {$mod: ['$ts', groupingPeriod]}]}, ts]
+          }
+        }
+      )
+      .toArray()
+  ).map((d) => ({version: d.version, platform: d.platform, count: d.count || 1}));
+
+  const data3 = data2.reduce((acc, curr) => {
+    const idx = acc.findIndex((d) => d.version === curr.version && d.platform === curr.platform);
+    if (idx === -1) {
+      acc.push({...curr, ts: ts});
+    } else {
+      acc[idx].count += curr.count;
+    }
+    return acc;
+  }, [] as doc[]);
+
+  console.log(
+    await collection.deleteMany({
+      $expr: {
+        $eq: [{$subtract: ['$ts', {$mod: ['$ts', groupingPeriod]}]}, ts]
+      }
+    })
+  );
+
+  console.log(
+    await collection.insertMany(data3).then((r) => {
+      const {insertedIds, ...x} = r;
+      return x;
+    })
+  );
+  console.log('processed', new Date(ts).toLocaleString());
+  return await cleanupDB(groupingPeriod);
+};
+
+cleanupDB(30 * 60 * 1000)
+  .then(() => cleanupDB(60 * 1000))
+  .then((client)=> client.close());
---
+++ mongodb-client.ts
@@ -0,0 +1,15 @@
+// Import the dependency.
+import { MongoClient } from 'mongodb';
+import { doc } from './mongodb-cleanup';
+const uri = process.env.MONGODB_URI;
+const options:any = {
+   useUnifiedTopology: true,
+   useNewUrlParser: true,
+};
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+client = new MongoClient(uri, options);
+const collectionPromise = client.connect();
+
+export default collectionPromise;
