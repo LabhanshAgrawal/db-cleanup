2e086d2851d8883a1fd673df21620165ff763ac7
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-09-24 10:20:43 +0530

    v3

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
-setInterval(() => {
-    console.log(`${new Date().toLocaleTimeString()} - Hello World!`);
-}, 30_000);
+// create an express app
+import express from "express"
+const app = express()
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
